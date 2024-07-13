2bec575d998ed3e7b6cb19312281ba26ee0b44e3
RenanSouza <renansouza0122@gmail.com>
2024-07-13 18:04:23 -0300

    :sparkles: feat: Get method

--- src/main/java/com/renan/forum_hub/controller/TopicController.java
+++ src/main/java/com/renan/forum_hub/controller/TopicController.java
@@ -1,6 +1,8 @@
 package com.renan.forum_hub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,4 +26,17 @@ public class TopicController {
 
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping
+    public ResponseEntity<Page<TopicDTO>> getAllTopics(Pageable pagination){
+    var page =  this.repository.findAll(pagination).map(TopicDTO::new);
+    return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getDetailTopic(@PathVariable Long id){
+        var topic = this.repository.getReferenceById(id);
+        return ResponseEntity.ok(new TopicDTO(topic));
+    }
+
 }
--- src/main/java/com/renan/forum_hub/dto/TopicDTO.java
+++ src/main/java/com/renan/forum_hub/dto/TopicDTO.java
@@ -2,14 +2,23 @@ package com.renan.forum_hub.dto;
 
 import java.time.LocalDateTime;
 
+import com.renan.forum_hub.domain.topic.Topic;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record TopicDTO(
-                @NotBlank String title,
-                @NotBlank  String message,
-                @NotNull   LocalDateTime create_at,
-                @NotBlank String author,
-                @NotBlank  String course,
-                @NotBlank String state
-               ) {}
+
+        Long id,
+        @NotBlank String title,
+        @NotBlank String message,
+        @NotNull LocalDateTime create_at,
+        @NotBlank String author,
+        @NotBlank String course,
+        @NotBlank String state) {
+
+    public TopicDTO(Topic topic) {
+        this(topic.getId(),topic.getTitle(), topic.getMessage(), topic.getCreate_at(), topic.getAuthor(),topic.getCourse(), topic.getState());
+    }
+
+}
