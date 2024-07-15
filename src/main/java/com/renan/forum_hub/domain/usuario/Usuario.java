05a75ed72250c1a203bacec9f093318f37130dd5
RenanSouza <renansouza0122@gmail.com>
2024-07-14 23:07:56 -0300

    :sparkles: feat: login and password

--- src/main/java/com/renan/forum_hub/domain/usuario/Usuario.java
+++ src/main/java/com/renan/forum_hub/domain/usuario/Usuario.java
@@ -1,6 +1,13 @@
 package com.renan.forum_hub.domain.usuario;
 
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +21,23 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails{
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     private String login;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public String getUsername() {
+        return login;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
 }
---
+++ src/main/java/com/renan/forum_hub/dto/AuthenticationDTO.java
@@ -0,0 +1,5 @@
+package com.renan.forum_hub.dto;
+
+public record AuthenticationDTO(String login,String password) {
+    
+}
